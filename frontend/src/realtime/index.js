7059063206a6f1e74692365a47afc179abc4c110
AhmedHS <ahmed.h.soliman@outlook.com>
2019-01-26 17:07:01 -0500

    reformatting

--- frontend/src/realtime/index.js
+++ frontend/src/realtime/index.js
@@ -9,57 +9,57 @@ const RealTime = () => {
         {quarter: 3, earnings: 14250},
         {quarter: 4, earnings: 19000}
       ];
-    const speedData = [
-        { x: 1, y: 2},
-        { x: 2, y: 3},
-        { x: 3, y: 5},
-        { x: 4, y: 4},
-        { x: 5, y: 6}
-    ]
   return (
-    <div id="grid-container">
-        <svg style={{ height: 0 }}>
-            <defs>
-                <linearGradient id="talk-data">
-                    <stop offset="0%" stopColor="#BDFFF3"/>
-                    <stop offset="90%" stopColor="#4AC29A"/>
-                </linearGradient>
-            </defs>
-        </svg>
-        <div className="grid-item-top">
-            Body language
+      <div id="realtime">
+        <div id="grid-container">
+            <svg style={{ height: 0 }}>
+                <defs>
+                    <linearGradient id="talk-data">
+                        <stop offset="0%" stopColor="#BDFFF3"/>
+                        <stop offset="90%" stopColor="#4AC29A"/>
+                    </linearGradient>
+                </defs>
+            </svg>
+            <div className="grid-item-top">
+                Body language
+                <div id="body-language">
+                    <span>Score:</span>
+                    <VictoryPie 
+                        startAngle={60}
+                        endAngle={-90}
+                        colorScale={["url(#talk-data)"]}
+                        data={[1]}
+                        labels={() => null}
+                    />
+                </div>
+            </div>
+            <div className="grid-item-top">WebCam footage</div>
+            <div className="grid-item-top">Facial Expressions</div>
+            <div className="grid-item-bottom"><div>300</div> Filler Words used
 
-            <VictoryPie
-                startAngle={90}
-                endAngle={-90}
-                colorScale={["url(#talk-data)"]}
-                data={[1]}
-            />
-
+            </div>
+            <div className="grid-item-bottom">
+                Talking speed
+                <VictoryChart>
+                    <VictoryArea
+                    interpolation="natural"
+                    style={{
+                        data: {fill: "url(#talk-data)"}
+                    }}
+                    data={[
+                        { x: 1, y: 2 },
+                        { x: 2, y: 3 },
+                        { x: 3, y: 7 },
+                        { x: 4, y: 4 },
+                        { x: 5, y: 5 }
+                    ]}
+                    />
+                </VictoryChart>
+            </div>
+            <div className="grid-item-bottom">Voice
+            
+            </div>
         </div>
-        <div className="grid-item-top">WebCam footage</div>
-        <div className="grid-item-top">Facial Expressions</div>
-        <div className="grid-item-bottom">Filler Words</div>
-        <div className="grid-item-bottom">
-            Talking speed
-            <VictoryChart>
-                <VictoryArea
-                interpolation="natural"
-                style={{
-                    data: {fill: "url(#talk-data)"}
-                }}
-                data={[
-                    { x: 1, y: 2 },
-                    { x: 2, y: 3 },
-                    { x: 3, y: 7 },
-                    { x: 4, y: 4 },
-                    { x: 5, y: 5 }
-                ]}
-                />
-            </VictoryChart>
-        </div>
-        <div className="grid-item-bottom">Voice</div>
-
     </div>
   );
 };
