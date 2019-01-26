70dd9cd7f390bb8bd8585dd979b7a6041941830b
nirmal <nirmalvettiankal@gmail.com>
2019-01-26 18:19:51 -0500

    multithread

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template, jsonify, request, abort
 from audio import Audio
 from video import Video
+from threading import Thread
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
 SPEECH = None
+def start_speech():
+    global SPEECH
+    SPEECH = Audio()
+
 VISION = None
+def start_vision():
+    global VISION
+    VISION = Video()
 
 
 @app.route('/', methods=['GET'])
@@ -17,9 +25,8 @@ def index():
 
 @app.route('/start', methods=['POST'])
 def start_recording():
-    global SPEECH, VISION
-    SPEECH = Audio()
-    VISION = Video()
+    Thread(target=start_speech).start()
+    Thread(target=start_vision).start()
     return jsonify(success=True)
 
 
