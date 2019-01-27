87addd0030d9f515fc30a84122cd9860b6bde065
AhmedHS <ahmed.h.soliman@outlook.com>
2019-01-27 05:43:21 -0500

    possible fix to chart issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
     xhr.onreadystatechange = function() {
       if (xhr.readyState == XMLHttpRequest.DONE) {
         let copySpeed = this.state.currentTalkSpeed;
-        copySpeed.push({x: this.state.currentTalkSpeed.length+1, y: JSON.parse(xhr.responseText).data.audio.wpm_by_line[0]})
+        copySpeed.push({x: this.state.currentTalkSpeed.length+1, y: JSON.parse(xhr.responseText).data.audio.wpm_by_line[this.state.currentTalkSpeed.length-1]})
         console.log(copySpeed);
         this.setState({
           load: JSON.parse(xhr.responseText).data,
